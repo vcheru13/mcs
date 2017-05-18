fa2a26d2e283a90972739d76e3da920f53929e69
Venkat Cherukupalli <vcheru@gmail.com>
2017-05-17 21:23:58 -0400

    Updated config for mac

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 #
 
 # Managed Xen hosts list 
-HOSTS = ['cvkvm.lab.local']
+HOSTS = ['myxen.lab.local']
 
 # MAC Address list db file
 MACDB = '.mcs_mac_db'
 
 # PKI cert location dir
-PKIPATH = '/home/cv/git/.pki'
+PKIPATH = '/var/tmp/cv/git/.pki'
 
 
 

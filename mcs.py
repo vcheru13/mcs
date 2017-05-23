57f4913338e39853ad56b66a0fa66fb82a83c2ad
vcheru13 <vcheru@gmail.com>
2017-05-22 22:23:29 -0400

    basics and start/stop domains functionality is working at this stage

--- mcs.py
+++ mcs.py
@@ -11,27 +11,63 @@
 import argparse
 import sys
 import requests
+import json
 
-mcs_server_url = 'http://127.0.0.1:10080/'
+mcs_server_url = 'http://127.0.0.1:10080/mcs/api/v0.1'
 
+def getmcsapi(mcsurl):
+    r = requests.get(mcsurl)
+    if r.status_code == 200:
+        if isinstance(r.json(),dict):
+            for k in r.json():
+                print k,'==>',
+                print json.dumps(r.json().get(k),sort_keys=True,indent=4)
+        else:
+            for k in r.json():
+                print k
+    else:
+        print 'ERROR: unable to GET ', mcsurl
+
+def putmcsapi(mcsurl,payload):
+    r = requests.put(mcsurl,json=payload)
+    if r.status_code == 200:
+        print json.dumps(r.json(),sort_keys=True,indent=4)
+    else:
+        print 'ERROR: unable to PUT ', mcsurl
+
+def postmcsapi(mcsurl,payload):
+    r = requests.post(mcsurl,json=payload)
+    if r.status_code == 200:
+        print json.dumps(r.json(),sort_keys=True,indent=4)
+    else:
+        print 'ERROR: unable to POST ', mcsurl
 
 def gethost(hname):
     print 'Parsing info of host: ', hname
+    getmcsapi(mcs_server_url + '/hosts/' + hname )
 
 def getallhosts():
     print 'All hosts info'
+    getmcsapi(mcs_server_url + '/hosts')
 
 def getdomains(hname):
     print 'All domains on: ', hname
+    getmcsapi(mcs_server_url + '/hosts/' + hname + '/domains')
+    sys.exit()
 
 def getdominfo(hname,dname):
     print 'Domain info ', dname, ' on: ', hname
+    getmcsapi(mcs_server_url + '/hosts/' + hname + '/domains/' + dname)
 
 def domstart(hname,dname):
     print 'Starting domain ', dname, ' on: ', hname
+    payload = { "cmd": "start", "args": None }
+    putmcsapi(mcs_server_url + '/hosts/' + hname + '/domains/' + dname,payload)
                              
 def domstop(hname,dname):
     print 'Stoping domain ', dname, ' on: ', hname
+    payload = { "cmd": "stop", "args": None }
+    putmcsapi(mcs_server_url + '/hosts/' + hname + '/domains/' + dname,payload)
 
 def domcreate(hname,dname,opts):
     print 'Creating domain ', dname, ' on: ', hname
@@ -40,6 +76,7 @@ def domdestroy(hname,dname):
     print 'Destroying domain ', dname, ' on: ', hname
 
 usage='''
+
  mcs host
  mcs host --info <hname>
  mcs domain --host <hanme>
@@ -48,6 +85,7 @@ usage='''
  mcs domain --cmd stop  --host <hanme> --domu <dname>
  mcs domain --cmd create --host <hanme> --opts={key1=value1,key2=value2...}
  mcs domain --cmd destroy --host <hanme> --domu <dname>
+
  ''' 
 
 parser = argparse.ArgumentParser(usage=usage,add_help=False)
@@ -68,10 +106,13 @@ else:
     if not args.hname:
         print 'hostname required' + usage
         sys.exit(1)
-    if not args.dname:
-        print 'domain required' + usage
-        sys.exit(1)
-    if not args.cmd:
+
+    if not args.dname and not args.cmd:
+        # get all domains on host
+        getdomains(args.hname)
+
+    if args.dname and not args.cmd:
+        # get domain info on host
         getdominfo(args.hname,args.dname)
     else: 
         if args.cmd == 'create' and not args.opts:
@@ -81,8 +122,8 @@ else:
             #print 'Running command: ' + args.cmd + ' for domain: ' + args.dname + ' on host: ' + args.hname 
             #if args.opts:
             #    print 'Opts: '  + args.opts
-            domcmd_funcs = { 'start': domstart(args.hname,args.dname),
-                             'stop': domstop(args.hname,args.dname),
-                             'create': domcreate(args.hname,args.dname,args.opts),
-                             'destroy': domdestroy(args.hname,args.dname) }
-            domcmd_funcs[args.cmd]
+            domcmd_funcs = { 'start': domstart, 'stop': domstop, 'create': domcreate, 'destroy': domdestroy }
+            if args.opts:
+                domcmd_funcs[args.cmd](args.hname,args.dname,args.opts)
+            else:
+                domcmd_funcs[args.cmd](args.hname,args.dname)
